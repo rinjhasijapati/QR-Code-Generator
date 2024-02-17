d79f451a5e80afa3a686715294916121b4921c69
rinjhasijapati <aastha_sijapati@yahoo.com>
2024-02-17 22:21:40 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import InputForm from "./components/InputForm";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-screen pt-36 px-2'>
+      <div className="container mx-auto max-w-4xl bg-white rounded-md shadow">
+        <div className="md:grid md:grid-cols-3">
+          <InputForm />
+          <h1>QR Code</h1>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+/*19:56*/
---
+++ src/components/InputForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import InputField from './InputField';
+import InputColor from './InputColor';
+
+const InputForm = () => {
+  return (
+    <div className='col-span-2 p-6 grid gap-4'>
+        <InputField />
+        <InputColor />
+        <button className='bg-blue-400 max-w-xs ml-auto px-4 py-2 text-white rounded-sm mt-4 hover:bg-blue-500 disabled:bg-gray-300'>Generate QR Code</button>
+    </div>
+  )
+}
+
+export default InputForm
